82177719bbcbc288a56ac9b73c2c933b2887a8ae
diana_fedotova <diana.fedotova.2000@mail.ru>
2020-05-18 12:09:50 +0300

    forger password added

--- app/src/main/java/com/itis/group11801/fedotova/mvpexample/EmailSignInFragment.kt
+++ app/src/main/java/com/itis/group11801/fedotova/mvpexample/EmailSignInFragment.kt
@@ -1,16 +1,15 @@
 package com.itis.group11801.fedotova.mvpexample
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_email_sign_in.*
-import kotlinx.android.synthetic.main.fragment_email_sign_in.container
 
 class EmailSignInFragment : Fragment() {
 
@@ -31,6 +30,7 @@ class EmailSignInFragment : Fragment() {
 
     private fun initClickListeners() {
         btn_to_sign_up.setOnClickListener { findNavController().navigate(R.id.nav_sign_up) }
+        btn_reset_password.setOnClickListener { resetPassword() }
         btn_sign_in.setOnClickListener {
             auth.signInWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
                 .addOnCompleteListener {
@@ -44,8 +44,27 @@ class EmailSignInFragment : Fragment() {
         }
     }
 
+    private fun resetPassword() {
+        val email = et_email.text.toString()
+        if (email.isNotEmpty()) {
+            auth.sendPasswordResetEmail(email).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(activity, SEND_SUCCESS, Toast.LENGTH_LONG)
+                        .show()
+                } else {
+                    Toast.makeText(activity, SEND_FAILED, Toast.LENGTH_LONG).show()
+                }
+            }
+        } else {
+            Toast.makeText(activity, INCORRECT_EMAIL, Toast.LENGTH_LONG).show()
+        }
+    }
+
     companion object {
         private const val AUTH_SUCCESS = "Authentication success"
         private const val AUTH_FAILED = "Authentication failed"
+        private const val SEND_SUCCESS = "Check email to reset password"
+        private const val SEND_FAILED = "Failed"
+        private const val INCORRECT_EMAIL = "Incorrect email"
     }
 }
